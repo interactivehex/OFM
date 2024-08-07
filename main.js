74b803ca3ff3125ebca60b1126583c74c12b7104
HEX Interactive <hexinteractive00@gmail.com>
2024-08-08 00:18:52 +0300

    phone responsive

---
+++ main.js
@@ -0,0 +1,9 @@
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.top = "0vh"; // Adjust based on your nav height
+}
+
+function closemenu() {
+    sidemenu.style.top = "-50vh";
+}
