ab51df8defb99686c6858c13edfd6f7054126ca7
HEX Interactive <hexinteractive00@gmail.com>
2024-08-08 13:31:53 +0300

    useless edit

--- main.js
+++
@@ -1,9 +0,0 @@
-var sidemenu = document.getElementById("sidemenu");
-
-function openmenu() {
-    sidemenu.style.top = "0vh"; // Adjust based on your nav height
-}
-
-function closemenu() {
-    sidemenu.style.top = "-50vh";
-}
